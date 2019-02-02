349bf3ae4bc6f01f3c9b19be8d0600a6a75c0793
wuxiangfei <wuxiangfei@kingsoft.com>
2019-02-02 18:15:04 +0800

    modified:   XMLTools.sln 	modified:   XMLTools.vcxproj 	new file:   pretty/pretty/Resource.h 	new file:   pretty/pretty/pretty.cpp 	new file:   pretty/pretty/pretty.h 	new file:   pretty/pretty/pretty.rc 	new file:   pretty/pretty/pretty.vcxproj 	new file:   pretty/pretty/prettyDlg.cpp 	new file:   pretty/pretty/prettyDlg.h 	new file:   pretty/pretty/res/pretty.ico 	new file:   pretty/pretty/res/pretty.rc2 	new file:   pretty/pretty/stdafx.cpp 	new file:   pretty/pretty/stdafx.h 	new file:   pretty/pretty/targetver.h 	new file:   x64/Debug/install_pretty.reg

---
+++ pretty/pretty/pretty.h
@@ -0,0 +1,32 @@
+﻿
+// pretty.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CPrettyApp:
+// 有关此类的实现，请参阅 pretty.cpp
+//
+
+class CPrettyApp : public CWinApp
+{
+public:
+	CPrettyApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPrettyApp theApp;
